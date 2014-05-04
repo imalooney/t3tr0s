502d35a08b8f3d2a08dbb64c82fdf5fcb8e2b4c7
Shaun Williams <swilliams@pros.com>
2014-05-04 15:46:04 -0500

    create pieces, board, and functions for printing board

--- src/tet/core.cljs
+++ src/tet/core.cljs
@@ -1,5 +1,60 @@
-(ns tet.core)
+(ns tet.core
+  (:require
+    [clojure.string :refer [join]]))
 
 (enable-console-print!)
 
-(println "Hello world!")
+(def pieces
+  {:I [[0,0],[0,-1],[0,1],[0,2]]
+   :L [[0,0],[0,1],[1,1],[0,-1]]
+   :J [[0,0],[0,-1],[0,1],[-1,1]]
+   :S [[0,0],[-1,0],[0,-1],[1,-1]]
+   :Z [[0,0],[-1,-1],[0,-1],[1,0]]
+   :O [[0,0],[-1,0],[-1,1],[0,1]]
+   :T [[0,0],[-1,0],[1,0],[0,1]]})
+
+(def state (atom {:board [[0,0,0,0,0,0,0,0,0,0]
+                          [0,0,0,0,0,0,0,0,0,0]
+                          [0,0,0,0,0,0,0,0,0,0]
+                          [0,0,0,0,0,0,0,0,0,0]
+                          [0,0,1,1,1,1,0,0,0,0]
+                          [0,0,1,0,0,1,0,0,0,0]
+                          [0,0,1,0,0,1,0,0,0,0]
+                          [0,0,1,0,0,1,0,0,0,0]
+                          [0,0,1,1,1,1,0,0,0,0]
+                          [0,0,1,0,0,0,0,0,0,0]
+                          [0,0,1,0,0,0,0,0,0,0]
+                          [0,0,1,0,0,0,0,0,0,0]
+                          [0,0,1,0,0,0,0,0,0,0]
+                          [0,0,1,0,0,0,0,0,0,0]
+                          [0,0,1,0,0,0,0,0,0,0]
+                          [0,0,1,0,0,0,0,0,0,0]
+                          [0,0,0,0,0,0,0,0,0,0]
+                          [0,0,0,0,0,0,0,0,0,0]
+                          [0,0,0,0,0,0,0,0,0,0]
+                          [0,0,0,0,0,0,0,0,0,0]
+                          [0,0,0,0,0,0,0,0,0,0]
+                          [0,0,0,0,0,0,0,0,0,0]]}))
+
+(defn get-cell-str
+  "Return a space or asterisk for the given cell."
+  [cell]
+  (if (= 0 cell) "_" "*"))
+
+(defn row-str
+  "Create a string from a board row."
+  [row]
+  (join "" (map get-cell-str row)))
+
+(defn board-str
+  "Create a string from the tetris board."
+  [board]
+  (join "\n" (map row-str board)))
+
+(defn print-board []
+  (println (board-str (:board @state))))
+
+(defn init []
+  (print-board))
+
+(.addEventListener js/window "load" init)
