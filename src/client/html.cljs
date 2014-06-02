503c0ab0dfd76869f7725f9168798293c12b8734
Elaine Looney <imalooney@gmail.com>
2014-06-02 00:09:02 -0500

    clean up home page layout

--- src/client/html.cljs
+++ src/client/html.cljs
@@ -7,14 +7,13 @@
 	(.getElementById js/document id))
 
 (hiccups/defhtml home-page []
-	[:div {:style "width:100%;"}
-		[:div {:style "width:700px;"}
-			[:img {:src "img/t3tr0s_500w.png"}]
-			[:div
-				[:div.lg-btn-container
-					[:button.lg-btn "SOLO"]]				
-				[:div.lg-btn-container
-					[:button.lg-btn "LOBBY"]]]]])
+	[:div#inner-container
+		[:img.logo {:src "img/t3tr0s_500w.png"}]
+		[:div
+			[:div.lg-btn-container
+				[:button.lg-btn "SOLO"]]				
+			[:div.lg-btn-container
+				[:button.lg-btn "LOBBY"]]]])
 
 (defn ^:export homeInit []
 	(let [container (by-id "main-container")] 
