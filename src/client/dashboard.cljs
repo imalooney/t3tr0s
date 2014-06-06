f0e0f7c25a139a612be28724eba30514a2d46b11
Shaun Williams <swilliams@pros.com>
2014-06-06 13:42:22 -0500

    add player board rendering to dashboard

--- src/client/dashboard.cljs
+++ src/client/dashboard.cljs
@@ -1,10 +1,14 @@
 (ns client.dashboard
   (:require-macros [hiccups.core :as hiccups])
   (:require
+    [client.game.paint :refer [draw-board! size-canvas!]]
+    [client.game.board :refer [empty-board rows-cutoff]]
     [client.util :as util]
     [cljs.reader :refer [read-string]]
     [client.socket :refer [socket]]
     hiccups.runtime))
+
+(def cell-size 24)
 
 ;; TODO:
 ;; - hide empty boards that are not in the leaderboard 10
@@ -33,126 +37,35 @@
 
 (defn- create-board-if-needed! [id]
   (if-not (by-id id)
-    (.append ($ "#boardsContainer") (board id))))
+    (.append ($ "#boardsContainer") (board id))
+    (size-canvas! (str "canvas-" id) empty-board cell-size rows-cutoff)
+    ))
 
 ;;------------------------------------------------------------------------------
 ;; Leaderboard State
 ;;------------------------------------------------------------------------------
 
-(def leaders1 [
-  { :name "Chris"
-    :board {}
-    :score 282372 }
-  { :name "Shaun"
-    :board {}
-    :score 232323 }
-  { :name "Elaine"
-    :board {}
-    :score 232323 }
-  { :name "Luis"
-    :board {}
-    :score 88 }
-  { :name "Phil"
-    :board {}
-    :score 8282 }
-  { :name "Rose"
-    :board {}
-    :score 746289 }
-  { :name "Brian"
-    :board {}
-    :score 23232 }
-  { :name "Brett"
-    :board {}
-    :score 882922 }
-  { :name "Andrew"
-    :board {}
-    :score 99723 }
-  { :name "Andy"
-    :board {}
-    :score 998237 }
-  ])
-
-(def leaders2 [
-  { :name "Shaun"
-    :board {}
-    :score 232323 }
-  { :name "Chris"
-    :board {}
-    :score 282372 }
-  { :name "Elaine"
-    :board {}
-    :score 232323 }
-  { :name "Luis"
-    :board {}
-    :score 88 }
-  { :name "Phil"
-    :board {}
-    :score 8282 }
-  { :name "Rose"
-    :board {}
-    :score 746289 }
-  { :name "Brian"
-    :board {}
-    :score 23232 }
-  { :name "Brett"
-    :board {}
-    :score 882922 }
-  { :name "Andrew"
-    :board {}
-    :score 99723 }
-  { :name "Andy"
-    :board {}
-    :score 998237 }
-  ])
-
-(def leaders (atom [
-  { :name "Chris"
-    :board {}
-    :score 282372 }
-  { :name "Elaine"
-    :board {}
-    :score 232323 }
-  { :name "Shaun"
-    :board {}
-    :score 232323 }
-  { :name "Luis"
-    :board {}
-    :score 88 }
-  { :name "Phil"
-    :board {}
-    :score 8282 }
-  { :name "Rose"
-    :board {}
-    :score 746289 }
-  { :name "Brian"
-    :board {}
-    :score 23232 }
-  { :name "Brett"
-    :board {}
-    :score 882922 }
-  { :name "Andrew"
-    :board {}
-    :score 99723 }
-  { :name "Andy"
-    :board {}
-    :score 998237 }
-  ]))
+(def leaders (atom []))
 
 (def place-classes (str "place-1 place-2 place-3 place-4 place-5"
   " place-6 place-7 place-8 place-9 place-10"))
 
 (defn- update-board [idx itm]
-  (create-uuid-if-needed! (:name itm))
-  (create-board-if-needed! (get @ids (:name itm)))
+  (create-uuid-if-needed! (:pid itm))
+  (create-board-if-needed! (get @ids (:pid itm)))
   (let [place (+ idx 1)
-        id (get @ids (:name itm))
+        id (get @ids (:pid itm))
         $el ($ (by-id id))]
+
     (.removeClass $el place-classes)
     (.addClass $el (str "place-" place))
-    (.html ($ (str "#" id " .name-1d96a")) (:name itm))
+
+    (.html ($ (str "#" id " .name-1d96a")) (:user itm))
     (.html ($ (str "#" id " .score-5aeae")) (:score itm))
-    ;; TODO: update the board right here, the element is:
-    ;; (str "#" id ".board-45de4")
+    (js/console.log (:score itm))
+
+    (draw-board! (str "canvas-" id) (:board itm) cell-size (:theme itm) rows-cutoff)
+
     ))
 
 (defn- on-change-leaders [_ _ _ new-leaders]
@@ -172,7 +85,8 @@
 (hiccups/defhtml board [id]
   [:div.board-a3a91 {:id id}
     [:div.name-1d96a]
-    [:div.board-45de4]
+    [:div.board-45de4
+     [:canvas.canvas-ad036 {:id (str "canvas-" id)}]]
     [:div.score-5aeae]])
 
 (hiccups/defhtml dashboard-html []
@@ -199,22 +113,7 @@
 (defn on-leader-update
   [str-data]
   (let [data (read-string str-data)]
-    nil)
-  )
-
-;;------------------------------------------------------------------------------
-;; Events
-;;------------------------------------------------------------------------------
-
-(defn- click-test1 []
-  (reset! leaders leaders1))
-
-(defn- click-test2 []
-  (reset! leaders leaders2))
-
-(defn- add-events []
-  (.on ($ "#btn1") "click" click-test1)
-  (.on ($ "#btn2") "click" click-test2))
+    (reset! leaders data)))
 
 ;;------------------------------------------------------------------------------
 ;; Page Initialization / Cleanup
@@ -223,7 +122,6 @@
 (defn init []
   (client.core/set-bw-background!)
   (.html ($ "#main-container") (dashboard-html))
-  (add-events)
   (swap! leaders identity)
 
   (.emit @socket "join-dashboard")
