92d79fc8c914c7bbc642939686ef17868312833e
Chris Oakman <chris@oakmac.com>
2014-06-05 22:18:18 -0500

    more work on dashboard; still needs some CSS

--- src/client/dashboard.cljs
+++ src/client/dashboard.cljs
@@ -4,6 +4,10 @@
     [client.socket :refer [socket]]
     hiccups.runtime))
 
+;; TODO:
+;; - hide empty boards that are not in the leaderboard 10
+;; - more efficient board updating code?
+
 (def $ js/jQuery)
 
 ;; TODO: move this to a util namespace
@@ -36,18 +40,24 @@
   "Stores a mapping of username --> UUID for use as DOM ids"
   (atom {}))
 
-(defn create-uuid-if-needed
+(defn create-uuid-if-needed!
   "creates a mapping of username --> UUID in the ids atom if one does not already exist"
-  [l]
-  (if-not (get @ids (:name l))
-    (swap! ids (fn [old-ids]
-      (assoc old-ids (:name l) (uuid))))))
+  [n]
+  (if-not (get @ids n)
+    (swap! ids #(assoc % n (uuid)))))
+
+(defn- by-id [id]
+  (.getElementById js/document id))
+
+(defn- create-board-if-needed! [id]
+  (if-not (by-id id)
+    (.append ($ "#boardsContainer") (board id))))
 
 ;;------------------------------------------------------------------------------
 ;; Leaderboard State
 ;;------------------------------------------------------------------------------
 
-(def leaders (atom [
+(def leaders1 [
   { :name "Chris"
     :board {}
     :score 282372 }
@@ -78,22 +88,98 @@
   { :name "Andy"
     :board {}
     :score 998237 }
+  ])
+
+(def leaders2 [
+  { :name "Shaun"
+    :board {}
+    :score 232323 }
+  { :name "Chris"
+    :board {}
+    :score 282372 }
+  { :name "Elaine"
+    :board {}
+    :score 232323 }
+  { :name "Luis"
+    :board {}
+    :score 88 }
+  { :name "Phil"
+    :board {}
+    :score 8282 }
+  { :name "Rose"
+    :board {}
+    :score 746289 }
+  { :name "Brian"
+    :board {}
+    :score 23232 }
+  { :name "Brett"
+    :board {}
+    :score 882922 }
+  { :name "Andrew"
+    :board {}
+    :score 99723 }
+  { :name "Andy"
+    :board {}
+    :score 998237 }
+  ])
+
+(def leaders (atom [
+  { :name "Chris"
+    :board {}
+    :score 282372 }
+  { :name "Elaine"
+    :board {}
+    :score 232323 }
+  { :name "Shaun"
+    :board {}
+    :score 232323 }
+  { :name "Luis"
+    :board {}
+    :score 88 }
+  { :name "Phil"
+    :board {}
+    :score 8282 }
+  { :name "Rose"
+    :board {}
+    :score 746289 }
+  { :name "Brian"
+    :board {}
+    :score 23232 }
+  { :name "Brett"
+    :board {}
+    :score 882922 }
+  { :name "Andrew"
+    :board {}
+    :score 99723 }
+  { :name "Andy"
+    :board {}
+    :score 998237 }
   ]))
 
-(def place-classes ".place-1")
+(def place-classes (str "place-1 place-2 place-3 place-4 place-5"
+  " place-6 place-7 place-8 place-9 place-10"))
 
 (defn- update-board [idx itm]
-  (let [place (+ idx 1)]
-    (log idx)
-    (log itm)
-    (log "------------")
+  (create-uuid-if-needed! (:name itm))
+  (create-board-if-needed! (get @ids (:name itm)))
+  (let [place (+ idx 1)
+        id (get @ids (:name itm))
+        $el ($ (by-id id))]
+    (.removeClass $el place-classes)
+    (.addClass $el (str "place-" place))
+    (.html ($ (str "#" id " .name-1d96a")) (:name itm))
+    (.html ($ (str "#" id " .score-5aeae")) (:score itm))
+    ;; TODO: update the board right here, the element is:
+    ;; (str "#" id ".board-45de4")
     ))
 
-(defn- on-change-leaders [_ _ o n]
-  (doall (map create-uuid-if-needed n))
-  (doall (map-indexed
-    update-board
-    n)))
+(defn- on-change-leaders [_ _ _ new-leaders]
+  (doall
+    (map-indexed
+      update-board
+      new-leaders))
+  ;; TODO: hide any .board-a3a91 elements that do not have a .place class
+  )
 
 (add-watch leaders :main on-change-leaders)
 
@@ -101,10 +187,10 @@
 ;; HTML
 ;;------------------------------------------------------------------------------
 
-(hiccups/defhtml board [b]
-  [:div.board-a3a91 {:id (:name b)}
-    [:div.name-1d96a (:name b)]
-    [:div.board-45de4 "the board"]
+(hiccups/defhtml board [id]
+  [:div.board-a3a91 {:id id}
+    [:div.name-1d96a]
+    [:div.board-45de4]
     [:div.score-5aeae]])
 
 (hiccups/defhtml dashboard-html []
@@ -112,18 +198,7 @@
     [:button#btn1 "Test 1"]
     [:button#btn2 "Test 2"]
     [:h2.time-left-eb709 "Time Left: 2:32"]
-    [:div.boards-ad07f
-      [:div#shaun.board-a3a91.place-1]
-      [:div#elaine.board-a3a91.place-2]
-      [:div#luis.board-a3a91.place-3]
-      [:div#phil.board-a3a91.place-4]
-      [:div#andrew.board-a3a91.place-5]
-      [:div#chris.board-a3a91.place-6]
-      [:div#brian.board-a3a91.place-7]
-      [:div#zaki.board-a3a91.place-8]
-      [:div#katie.board-a3a91.place-9]
-      [:div#brett.board-a3a91.place-10]
-
+    [:div#boardsContainer.boards-ad07f
       [:div.num-2b782.first-100e1 "1" [:sup "st"]]
       [:div.num-2b782.second-e09e1 "2" [:sup "nd"]]
       [:div.num-2b782.third-deef68 "3" [:sup "rd"]]
@@ -133,9 +208,7 @@
       [:div.num-2b782.seventh-84a13 "7" [:sup "th"]]
       [:div.num-2b782.eight-15b29 "8" [:sup "th"]]
       [:div.num-2b782.ninth-780fc "9" [:sup "th"]]
-      [:div.num-2b782.tenth-34b19 "10" [:sup "th"]]
-
-      ]])
+      [:div.num-2b782.tenth-34b19 "10" [:sup "th"]]]])
 
 ;;------------------------------------------------------------------------------
 ;; Socket Events
@@ -152,10 +225,10 @@
 ;;------------------------------------------------------------------------------
 
 (defn- click-test1 []
-  (swap! leaders reverse))
+  (reset! leaders leaders1))
 
 (defn- click-test2 []
-  nil)
+  (reset! leaders leaders2))
 
 (defn- add-events []
   (.on ($ "#btn1") "click" click-test1)
@@ -167,7 +240,9 @@
 
 (defn init []
   (.html ($ "#main-container") (dashboard-html))
-  (add-events))
+  (add-events)
+  (swap! leaders identity)
+  )
 
 (defn cleanup
   []
