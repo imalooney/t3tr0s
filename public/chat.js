9eada3bd174887ca598b581c299901b3a8dd17e4
Chris Oakman <chris@oakmac.com>
2014-06-22 00:14:42 -0500

    I dont think we need these files anymore

--- public/chat.js
+++
@@ -1,3 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
-goog.addDependency("../chat/core.js", ['chat.core'], ['cljs.core']);
